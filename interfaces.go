da58c2a80f5c2c14ad77ae29f6d96c5a6386e13d
adisingh007 <adipratapsingh.aps@gmail.com>
2021-07-26 00:33:51 +0530

    Dog and Cat structs now implement Noisy interface. Used a loop for both to make noise.

--- interfaces.go
+++ interfaces.go
@@ -10,11 +10,25 @@ type Dog struct {
 	name string
 }
 
-func (d Dog) makeNoise() string {
-	return d.name + " Barks"
+type Cat struct {
+	name string
+	age int
+	toy string
 }
+
+func (d Dog) makeNoise() string {
+	return fmt.Sprintf("%s barks", d.name)
+}
+
+func (c Cat) makeNoise() string {
+	return fmt.Sprintf("%d year old %s meows with his %s", c.age, c.name, c.toy)
+} 
 
 func main() {
 	myDog := Dog{"Matthieu"}
-	fmt.Println(myDog.makeNoise())
+	myCat := Cat{"Mottet", 40, "PHd"}
+	noisies := []Noisy{myDog, myCat}
+	for _,p := range noisies {
+		fmt.Println(p.makeNoise())
+	}
 }
