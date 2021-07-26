00eb2b04784ab51e2430cd063643a90ad0db12be
adisingh007 <adipratapsingh.aps@gmail.com>
2021-07-26 11:08:12 +0530

    Passed reference of a variable from a function. I feel like a 50 YO boomer who cannot get over C++. :P

--- variable_addresses.go
+++ variable_addresses.go
@@ -4,6 +4,24 @@ import "fmt"
 
 func main() {
 	i, j := 24, 2701
-	fmt.Println(i, j)
-	fmt.Println(&i, &j)
+	fmt.Println("i = ", i, "j = ", j)
+	fmt.Println("&i = ", &i, "&j = ", &j)
+
+	p := &i
+	fmt.Println("p = ", p)
+	fmt.Println("*p = ", *p)
+
+	fmt.Println(willUseMemoryHeap()) // Haha WTF is this? Why would this print the value of the object and not the real address?
+	fmt.Println(*willUseMemoryHeap())
+}
+
+type Person struct {
+	name string
+	age int
+}
+
+
+func willUseMemoryHeap() *Person {
+	somePersonInMemoryHeap := Person{"Some anonymous joe", 56}
+	return &somePersonInMemoryHeap
 }
