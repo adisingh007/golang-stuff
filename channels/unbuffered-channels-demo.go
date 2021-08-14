67a50c6f7c99e10a693b6d0fbf1888d297771875
adisingh007 <adipratapsingh.aps@gmail.com>
2021-08-15 03:38:21 +0530

    Writing and recieving from and to unbuffered channel

--- channels/unbuffered-channels-demo.go
+++ channels/unbuffered-channels-demo.go
@@ -8,13 +8,12 @@ import (
 func listen(ch chan string) {
 	fmt.Println(<- ch)
 	ch <- "No, I'm straight!"
-	time.Sleep(time.Second)
 }
 
 func main() {
 	ch := make(chan string)
 	go listen(ch)
 	ch <- "Do you speak french?"
-	time.Sleep(time.Second)
 	fmt.Println(<- ch)
+	time.Sleep(time.Second)
 }
